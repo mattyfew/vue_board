83552d4cabeade2e2b4a7cf1b73a3abb40af29eb
Matt Fewer <mattfewer@hotmail.com>
2018-01-04 11:28:40 +0100

    slowly configuring vue

--- public/script.js
+++ public/script.js
@@ -1,1 +1,25 @@
-console.log("script.js running!")
+(function(){
+
+
+    var app = new Vue({
+        el: 'main',
+
+        data: {},
+
+        mounted: function() {
+            console.log("running mounted")
+        },
+
+        methods: {
+            upload: function() {
+                console.log("running upload method")
+            }
+        }
+    })
+
+
+    addEventListener('hashchange',  function() {
+        app.showImage(location.hash.slice(1))
+    })
+
+})()
--- server.js
+++ server.js
@@ -4,4 +4,9 @@ const app = express()
 app.use(express.static('./public'))
 app.use(require('body-parser').json())
 
+app.get('/images', (req, res) => {
+    console.log("in /images");
+    res.json({ message: "workin on it" })
+})
+
 app.listen(process.env.PORT || 8080, () => console.log(`I'm listening.`))
