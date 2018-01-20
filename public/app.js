becf24be5c1f4193ea1b9b304ba140b830fe4d21
Matt Fewer <mattfewer@hotmail.com>
2018-01-20 17:21:20 +0100

    started again, axios fetching on mount and rendering

--- public/app.js
+++ public/app.js
@@ -3,19 +3,27 @@
 
     var app = new Vue({
         el: 'main',
-
-        data: {},
-
+        data: {
+            images: []
+        },
         mounted: function() {
             console.log("running mounted")
+            var vm = this
+            axios.get('/images')
+                .then(response => {
+                    vm.images = response.data.images
+                    console.log(vm.images);
+                })
+                .catch(e => console.log("There was an error with GET /image", e))
         },
-
         methods: {
             upload: function() {
                 console.log("running upload method")
             }
         }
     })
+
+
 
 
     addEventListener('hashchange',  function() {
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const app = express()
+const spicedPg = require('spiced-pg')
+const db = spicedPg(process.env.DATABASE_URL || require('./secrets').DATABASE_URL)
+
 
 app.use(express.static('./public'))
 app.use(require('body-parser').json())
 
 app.get('/images', (req, res) => {
-    console.log("in /images");
-    res.json({ message: "workin on it" })
+    const q = 'SELECT * FROM images'
+
+    db.query(q)
+        .then(results => res.json({ images: results.rows}) )
+        .catch(e => console.log('There was an error with GET /images', e) )
 })
 
 app.listen(process.env.PORT || 8080, () => console.log(`I'm listening.`))
